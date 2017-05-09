f787cbb756a5a8614fc234aeb10d0d806954ebea
Adam Borowski <kilobyte@angband.pl>
2017-05-09 17:01:30 +0200

    Switch to xterm256 palette.

--- 256.c
+++ 256.c
@@ -12,9 +12,10 @@ int col256_to_rgb(int i)
     else if (i < 232)
     {
         i -= 16;
-        return (i / 36 * 85 / 2) << 16
-             | (i / 6 % 6 * 85 / 2) << 8
-             | (i % 6 * 85 / 2);
+        int r = i / 36, g = i / 6 % 6, b = i % 6;
+        return (r ? r * 0x280000 + 0x370000 : 0)
+             | (g ? g * 0x002800 + 0x003700 : 0)
+             | (b ? b * 0x000028 + 0x000037 : 0);
     }
     // Grayscale ramp.
     else // i < 256
