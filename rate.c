a5225c4e5a8c6fa7e0294a184aa98350b095553b
Adam Borowski <kilobyte@angband.pl>
2017-05-14 16:40:29 +0200

    rate: color the output using colors being displayed.

--- rate.c
+++ rate.c
@@ -29,9 +29,13 @@ int main(int argc, char **argv)
         ss += ((long long)d) * d / 16777216;
     }
 
-    printf("%-15s maxd=%#-7.6g (#%06X vs %3d:#%06X) Σd²=%#-8.6g\n", argv[1],
+    printf("%-15s maxd=%#-7.6g (\e[38;2;%d;%d;%dm#%06X\e[0m vs \e[38;5;%dm%3d\e[0m:\e[38;2;%d;%d;%dm#%06X\e[0m) Σd²=%#-8.6g\n",
+        argv[1],
         maxe/16777216.0,
-        worst, nt[worst], col256_to_rgb(nt[worst]),
+        (worst>>16)&0xff, (worst>>8)&0xff, worst&0xff, worst,
+        nt[worst], nt[worst],
+        (col256_to_rgb(nt[worst])>>16)&0xff, (col256_to_rgb(nt[worst])>>8)&0xff, col256_to_rgb(nt[worst])&0xff,
+        col256_to_rgb(nt[worst]),
         ss/16777216.0);
 
     return 0;
